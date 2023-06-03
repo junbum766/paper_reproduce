b3e3b3c1e62cfe764069a28898fcd1f97fac25bf
junbum766 <junbum766@gmail.com>
2023-06-03 19:08:06 +0900

    fix

--- resnet/check_model.py
+++ resnet/check_model.py
@@ -1,12 +1,12 @@
 import torchsummary
 from resnet import Resnet34, Resnet50
 
-model1 = Resnet34(1000).to('cuda')
-model2 = Resnet50(1000).to('cuda')
+model1 = Resnet34(1000)
+model2 = Resnet50(1000)
 
 print("Resnet34 architecture: ")
 torchsummary.summary(model1, (3, 224, 224))
-print()
+print('-------------------------------------------------')
 
 print("Resnet50 architecture: ")
 torchsummary.summary(model2, (3, 224, 224))
